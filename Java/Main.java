39db4a017d5f1549700ca0539cc06ebd9c76bb86
Anna Scott <workwithpurpose@yahoo.com>
2021-10-05 12:31:15 -0600

    Check if Array ascending
    wordCount that returns a SortedMap<String, Integer> where keys are all different strings and values are the number of times that string appears in the array. The method takes an array of strings as input;
    printMap that prints all entries of the map ("key : value").
    wordCount that returns a SortedMap<String, Integer> where keys are all different strings and values are the number of times that string appears in the array. The method takes an array of strings as input;
    printMap that prints all entries of the map ("key : value").

--- Java/Main.java
+++ Java/Main.java
@@ -1,40 +1,19 @@
-import java.util.*;
-
-class MapUtils {
-
-    public static SortedMap<String, Integer> wordCount(String[] strings) {
-        // write your code here
-        SortedMap<String, Integer> myMap = new TreeMap<>();
-        int count = 1;
-        for (int i = 0; i < strings.length; i++) {
-            count = 1;
-            for (int j = i + 1; j < strings.length; j++) {
-                if (strings[i].equals(strings[j])) {
-                    count++;
-                }
-            }
-                if (!myMap.containsKey(strings[i])) {
-                    myMap.put(strings[i], count);
-                }
-
-        }
-        return myMap;
-    }
-
-    public static void printMap(Map<String, Integer> map) {
-        // write your code here
-        for (var entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-    }
-
-}
-
-/* Do not change code below */
-public class Main {
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] words = scanner.nextLine().split(" ");
-        MapUtils.printMap(MapUtils.wordCount(words));
+        // put your code here
+        boolean ascending = true;
+        Scanner scn = new Scanner(System.in);
+        scn.nextLine();
+        int initialValue = scn.nextInt();
+        while (scn.hasNext()) {
+            int next = scn.nextInt();
+            if (next - initialValue < 1) {
+                ascending = false;
+                break;
+            }
+            initialValue = next;
+        }
+        System.out.println(ascending);
     }
 }
