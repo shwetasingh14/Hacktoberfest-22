f69393399f04c2b2570be4348e215f8aa79810c4
Winner121353 <50233676+Winner121353@users.noreply.github.com>
2021-10-18 00:12:06 +0530

    Update OTPverify.py
    Added comments to the program

--- Python/OTPverify.py
+++ Python/OTPverify.py
@@ -6,11 +6,16 @@ import smtplib
 digits="0123456789"
 OTP=""
 for i in range(6):
-    OTP+=digits[math.floor(random.random()*10)]
+    OTP+=digits[math.floor(random.random()*10)] #Randomly assigns a digit from 0-9 
 otp = OTP + " is your OTP"
 msg= otp
+
+#Creates a SMTP instance
 s = smtplib.SMTP('smtp.gmail.com', 587)
+
+#Put SMTP connection in TLS mode
 s.starttls()
+
 s.login("Your Gmail Account", "You app password")
 emailid = input("Enter your email: ")
 s.sendmail('&&&&&&&&&&&',emailid,msg)
