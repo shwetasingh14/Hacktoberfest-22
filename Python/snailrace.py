038c5c6de9eacd08263721127f4d194ee5b7ade8
Winner121353 <50233676+Winner121353@users.noreply.github.com>
2021-10-19 22:56:18 +0530

    Update snailrace.py
    Added Comments to the Program.

--- Python/snailrace.py
+++ Python/snailrace.py
@@ -1,13 +1,17 @@
 import random
 import pygame
+
+# Initialize all imported pygame modules
 pygame.init()
 import tsk
 
 print("Player 1, use the UP ARROW to move your snail forward (it's red).")
 
 print("Player 2, use the W KEY to move your snail forward (it's blue).")
-w=pygame.display.set_mode([300,600])
+w=pygame.display.set_mode([300,600]) # Fixes Size of Game Screen
 w.fill((0,0,0))
+
+# Snails Structure
 snail_1_y=550
 snail_1=pygame.Rect(50,snail_1_y,40,50)
 pygame.draw.ellipse(w, (255,0,0), snail_1)
@@ -22,9 +26,11 @@ while animating:
 
     
     for event in pygame.event.get():
+        # exits the game 
         if event.type==pygame.QUIT:
             animating=False
             
+        # When presed 'W' Snail 1 will move forward     
         if tsk.get_key_pressed(pygame.K_w):
             snail_1_y-=3
             snail_1=pygame.Rect(50,snail_1_y,40,50)
@@ -34,7 +40,7 @@ while animating:
             
             
             
-            
+        # When 'UP ARROW' key pressed Snail 2 will move forward    
         if tsk.get_key_pressed(pygame.K_UP):
             snail_2_y-=3
             snail_2=pygame.Rect(200, snail_2_y, 40, 50)
@@ -46,6 +52,7 @@ while animating:
     pygame.time.wait(10)      
     pygame.display.flip()
     
+    # Winning Condition 
     if snail_1_y<=1:
         print("Snail 1 wins!")
         animating=False
