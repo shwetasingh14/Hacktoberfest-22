6c6e3cb2aaa43ffa922dbf96f78ed438df1e9fe0
lemuellloren <lempakz@yahoo.com>
2021-10-19 17:28:18 +0800

    update qoutes

--- JavaScript/random-qoute-generator/js/script.js
+++ JavaScript/random-qoute-generator/js/script.js
@@ -2,27 +2,27 @@
     const quotes = [
         {
             quote:
-              " Shallow men believe in luck. Strong men believe in cause and effect. ",
-            author: "Ralph Waldo Emerson"
+              " We grow fearless when we do the things we fear. ",
+            author: "Robin Sharma"
           },
           {
             quote:
-              " I can’t give you a sure-fire formula for success, but I can give you a formula for failure: try to please everybody all the time.",
-            author: "Herbert Bayard Swope"
+              " And, when you want something, all the universe conspires in helping you to achieve it. ",
+            author: "Paulo Coelho, 'The Alchemist'"
           },
           {
             quote:
-              " The ladder of success is never crowded at the top.",
-            author: "Napoleon Hill "
+              " When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one which has been opened for us.",
+            author: "Helen Keller"
           },
           {
-            quote: " It is better to fail in originality than to succeed in imitation.",
-            author: "Herman Melville"
+            quote: " If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced",
+            author: "Vincent Van Gogh"
           },
           {
             quote:
-              " If you have no critics you’ll likely have no success.",
-            author: "Malcolm X"
+              " Love is an untamed force. When we try to control it, it destroys us. When we try to imprison it, it enslaves us. When we try to understand it, it leaves us feeling lost and confused.",
+            author: "Paulo Coelho"
           },
           {
             quote:
